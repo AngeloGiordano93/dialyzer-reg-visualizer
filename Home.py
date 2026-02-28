880beb02fefecff8c17f1b9c4c9f36935ce43a42
AngeloGiordano93 <angelo.giordano993@gmail.com>
2026-02-28 19:50:47 +0100

    Changed description of model used to SVR
    Updated the description of the web application to reflect changes in the model used for dialyzer clearance computation.

--- Home.py
+++ Home.py
@@ -8,8 +8,8 @@ st.set_page_config(
 st.title("Welcome to the interactive dialyzer performance visualizer!")
 
 st.markdown("""
-This web application enables the visual exploration of how a dialyzer's performance—quantified by urea clearance—varies as a function of its operating and design parameters.
-\nThe dialyzer clearance is computed by a fourth-degree polynomial model trained on synthetic data generated with a first-principles one-dimensional model.
+This web application enables the visual exploration of how a dialyzer's performance (quantified by urea clearance) varies as a function of its operating and design parameters.
+\nThe dialyzer clearance is computed by a support vector regressor model trained on synthetic data generated with a CFD model of the dialyzer.
 
 **👈 Select one page from the side bar to begin:**
 - **Performance Visualizer**: Interact with the model parameters and visualize the clearance variation graphically in real time.
